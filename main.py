4a1853ed7b1920d0e9aeb4ecd4e3008c5d610823
Igor Tankov <75366889a@gmail.com>
2022-12-23 12:03:27 +0300

    Added ability to use grunt inside the chatbot

--- main.py
+++ main.py
@@ -10,18 +10,25 @@ from aiogram.types import InlineQuery, \
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=config.API_TOKEN)
+bot = Bot(token=config.API_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет, это хрю-хрю бот. Напиши любое сообщение сюда и оно будет хрюифицированно!\nЛибо воспользуйся <code>@hruhruubot сообщение</code> в любом чате.\n\nby @alpine & @tnkv_trolling")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(await hru.mainHru(message.text))
 
 
 @dp.inline_handler()
 async def inline_echo(inline_query: InlineQuery):
     text = inline_query.query
     result_id: str = hashlib.md5(text.encode()).hexdigest()
-    hruificated = await hru.mainHru(text)
     item = InlineQueryResultArticle(
         id=result_id,
-        title=f'Отправить {hruificated}',
+        title=f'Отправить {await hru.mainHru(text)}',
         input_message_content=types.InputTextMessageContent(
             message_text=hruificated)
     )
